31d7d5bd1026b576569d37268defbea895f559c5
Munir Ibrahim (InConsulting Inc.) <v-muibr@microsoft.com>
2018-02-07 13:50:31 -0800

    tested out reverse string

--- src/reverse_string.py
+++ src/reverse_string.py
@@ -6,11 +6,23 @@ Reverse a string in-place.
 
 def reverse_string(str):
 
-    lst_str = list(str)
+    if not isinstance(str, string):
+        raise TypeError('Must input a list to reverse')
 
+    lst_str = list(str)
     start = 0
     end = len(lst_str) - 1
 
     while start < end:
-        
+        lst_str[start], lst_str[end] = lst_str[end], lst_str[start]
+        start += 1
+        end -= 1
+    
+    return ''.join(lst_str)
 
+if __name__ == '__main__':
+    reverse_string(None) # raise TypeError
+    reverse_string([1, 2, 3]) # raise TypeError
+    reverse_string('12345') # '54321'
+    reverse_string('1') # '1'
+    reverse_string('13') # '31'
